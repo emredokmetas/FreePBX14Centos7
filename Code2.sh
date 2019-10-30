41fb1bedf45d009733f28fe664327ff38ae18d81
emredokmetas <emredokmetas_58@hotmail.com>
2019-10-30 15:34:16 +0300

    Update Code2.sh

--- Code2.sh
+++ Code2.sh
@@ -1,6 +1,6 @@
 yum -y update
 yum -y groupinstall core base "Development Tools"
-yum -y install lynx mariadb-server mariadb php php-mysql php-mbstring tftp-server httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear
+yum -y install lynx mariadb-server mariadb tftp-server httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git crontabs cronie cronie-anacron vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php56w php56w-pdo php56w-mysql php56w-mbstring php56w-pear php56w-process php56w-xml php56w-opcache php56w-ldap php56w-intl php56w-soap
 pear install Console_Getopt
 
 firewall-cmd --zone=public --add-port=80/tcp --permanent
